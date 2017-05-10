a0f3410889573eac1cbd1af3f75efda8c6e5fcc9
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-10 18:09:25 +0200

    Cache the data for an expire time, default 1h

--- src/web.js
+++ src/web.js
@@ -7,11 +7,21 @@ const client = require('dcontentful').createClient({
   space: process.env.SPACE_ID,
   accessToken: process.env.ACCESS_TOKEN,
 });
+const dcache = require('dcache')(require('dcache-memory')());
 app.get('/', async (req, res) => {
-  const data = await client.getEntry(process.env.ENTRY_ID);
-  if (req.get('Accept') === 'application/json') {
-    res.send(data);
-  } else {
-    res.render('index.html.twig', data);
-  }
+  dcache(
+    'data',
+    async (callback) => {
+      const data = await client.getEntry(process.env.ENTRY_ID);
+      callback(data);
+    },
+    (data) => {
+      if (req.get('Accept') === 'application/json') {
+        res.send(data);
+      } else {
+        res.render('index.html.twig', data);
+      }
+    },
+    process.env.EXPIRE || require('dparse-duration')('1h', 's')
+  );
 });
