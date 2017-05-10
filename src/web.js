0d1eb88e532b745091e9c485f18de07f210d60d4
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-10 12:57:49 +0200

    Move data to "contentful" and refresh the project

---
+++ src/web.js
@@ -0,0 +1,20 @@
+require('dotenv-safe').config();
+
+const app = require('dexpress')();
+app.use(require('express').static('www'));
+app.engine('twig', require('swig').renderFile);
+const client = require('dcontentful').createClient({
+  space: process.env.SPACE_ID,
+  accessToken: process.env.ACCESS_TOKEN,
+});
+app.get('/', async (req, res) => {
+
+  console.log('LOADING...');
+
+  const data = await client.getEntry(process.env.ENTRY_ID);
+  if (req.get('Accept') === 'application/json') {
+    res.send(data);
+  } else {
+    res.render('index.html.twig', data);
+  }
+});
