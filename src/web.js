140ad4ab6cf32558ab45172c1dea8c6ce0ecf50b
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-22 14:01:33 +0200

    Fix paths

--- src/web.js
+++ src/web.js
@@ -1,11 +1,16 @@
 require('dotenv-safe').config();
 
+const path = require('path');
+
 const express = require('express');
 const app = express();
 app.disable('x-powered-by');
 app.engine('twig', require('swig').renderFile);
+app.set('views', require('path').join(__dirname, '../views'));
 
-require('@dnode/middlewares')(app, [express.static('www')]);
+require('@dnode/middlewares')(app, [
+  express.static(path.join(__dirname, '../www'))]
+);
 
 const contentfulClient = require('@dnode/contentful')({
   accessToken: process.env.ACCESS_TOKEN,
